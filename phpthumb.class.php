17f3a07f09b81a1896202c7a4c8179cd902c75bc
James Heinrich <info@getid3.org>
2026-01-08 10:43:34 -0500

    curl_close deprecated in PHP8

--- phpthumb.class.php
+++ phpthumb.class.php
@@ -267,7 +267,7 @@ class phpthumb {
 	public $issafemode       = null;
 	public $php_memory_limit = null;
 
-	public $phpthumb_version = '1.7.24-202509221109';
+	public $phpthumb_version = '1.7.24-202601081042';
 
 	//////////////////////////////////////////////////////////////////////
 
